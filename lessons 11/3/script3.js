1e552d8193cd677f163fda3852a7ba921221d872
John Doe <paniuta.p@gmail.com>
2024-10-24 20:34:37 +0300

    lessons 11-3

---
+++ lessons 11/3/script3.js
@@ -0,0 +1,19 @@
+const photo = document.createElement("img");
+photo.alt = "photo";
+photo.className = "block__photo__img";
+const blockPhoto = document.querySelector(".block__photo");
+
+function createElements() {
+  let rondoms = Math.floor(Math.random() * (10 - 1) + 1);
+  photo.src = `./photo/${rondoms}.jpg`;
+  blockPhoto.append(photo);
+}
+
+function rondom() {
+  let interval = setInterval(() => createElements(), 1000);
+
+  setTimeout(() => {
+    clearInterval(interval);
+  }, 30000);
+}
+rondom();
