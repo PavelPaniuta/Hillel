af299acf0f4415f644c1a9813cfcbcbc04dd2854
John Doe <paniuta.p@gmail.com>
2024-11-25 08:17:09 +0200

    lessons 19

--- lessons 19/script.js
+++ lessons 19/script.js
@@ -14,7 +14,7 @@ const monthNames = [
 ];
 const date = new Date();
 const month = date.getMonth();
-const day = date.getDay();
+const day = date.getDate();
 const year = date.getFullYear();
 const hours = date.getHours();
 const minutes = date.getMinutes();
