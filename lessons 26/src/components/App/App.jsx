98609cfa2fb90bbf9aac0d1de3a6b9ec2403cf63
John Doe <paniuta.p@gmail.com>
2024-12-20 08:23:35 +0200

    lessons 26

--- lessons 26/src/components/App/App.jsx
+++ lessons 26/src/components/App/App.jsx
@@ -5,7 +5,6 @@ import ShowWinnerBtn from "../ShowWinnerBtn/ShowWinnerBtn";
 import DeleteRes from "../DeleteRes/DeleteRes";
 
 const App = () => {
-
   const [items, setItems] = useState(() => {
     const savedItems = localStorage.getItem("smile");
     return savedItems
@@ -19,57 +18,46 @@ const App = () => {
           { id: 6, smile: 0, content: "😇" },
         ];
   });
-  
-  
- const handleSmileClick = (id) => {
-  setItems ((prevItems) =>
-    prevItems.map((item) =>
+
+  const handleSmileClick = (id) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
         item.id === id ? { ...item, smile: item.smile + 1 } : item
-      ),
+      )
     );
-   
   };
 
- const deleteAllInfo = () => {
-    setItems( 
-       [
-        { id: 1, smile: 0, content: "😀" },
-        { id: 2, smile: 0, content: "🥰" },
-        { id: 3, smile: 0, content: "😍" },
-        { id: 4, smile: 0, content: "💘" },
-        { id: 5, smile: 0, content: "😚" },
-        { id: 6, smile: 0, content: "😇" },
-      ]
-    )
-    
-    
-  }
+  const deleteAllInfo = () => {
+    setItems([
+      { id: 1, smile: 0, content: "😀" },
+      { id: 2, smile: 0, content: "🥰" },
+      { id: 3, smile: 0, content: "😍" },
+      { id: 4, smile: 0, content: "💘" },
+      { id: 5, smile: 0, content: "😚" },
+      { id: 6, smile: 0, content: "😇" },
+    ]);
+  };
 
   useEffect(() => {
     const savedItems = localStorage.getItem("smile");
-    
+
     if (savedItems) {
       setItems(JSON.parse(savedItems));
     }
-    
   }, []);
 
-  useEffect (() => {localStorage.setItem("smile", JSON.stringify(items));
+  useEffect(() => {
+    localStorage.setItem("smile", JSON.stringify(items));
   }, [items]);
 
-
-    return (
-      <div className="wrapper">
-        <Header text={"Голосування за крутіщий смайл"} />
-        <Smile
-          items={items}
-          handleSmileClick={handleSmileClick}
-        />
-        <ShowWinnerBtn items={items} />
-        <DeleteRes deleteAllInfo={deleteAllInfo}/>
-      </div>
-    );
-  }
-
+  return (
+    <div className="wrapper">
+      <Header text="Голосування за крутіщий смайл" />
+      <Smile items={items} handleSmileClick={handleSmileClick} />
+      <ShowWinnerBtn items={items} />
+      <DeleteRes deleteAllInfo={deleteAllInfo} />
+    </div>
+  );
+};
 
 export default App;
